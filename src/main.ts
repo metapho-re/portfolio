14a19173c5e97e672adebd5ed6e4b38e3a5e1489
Pierre-Alain Castella <pierrealain.castella@gmail.com>
2025-11-21 14:13:35 +0100

    feat: Revamp overall website styling.

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,10 @@
 import { attachMenuNavigationEvents } from "./attach-menu-navigation-events";
+import { randomizeFooterSpots } from "./randomize-footer-spots";
 import { showHtmlContent } from "./show-html-content";
 import { styleBullets } from "./style-bullets";
 import "./style.css";
 
 attachMenuNavigationEvents();
+randomizeFooterSpots();
 styleBullets();
 showHtmlContent();
